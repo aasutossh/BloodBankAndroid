aca568dbae273d32e116afa55b04aeaea3bef2fb
Aasutosh Jha <aasutossh@gmail.com>
2019-08-02 09:20:49 +0545

    New blank activity initialized.

---
+++ app/src/androidTest/java/np/com/aasutosh/bloodbankandroid/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package np.com.aasutosh.bloodbankandroid;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("np.com.aasutosh.bloodbankandroid", appContext.getPackageName());
+    }
+}
