6c798e95f524792966b7800cee46f6dd4f7e4669
Aasutosh Jha <aasutossh@gmail.com>
2019-08-04 12:34:32 +0545

    added activity java file for donate and request

---
+++ app/src/main/java/np/com/aasutosh/bloodbankandroid/DonateBloodActivity.java
@@ -0,0 +1,14 @@
+package np.com.aasutosh.bloodbankandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonateBloodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate_blood);
+    }
+}
