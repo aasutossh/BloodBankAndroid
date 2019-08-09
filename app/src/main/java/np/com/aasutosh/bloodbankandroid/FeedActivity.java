b88531ee4557a4f8d134fbd8df2a7fadb552a612
Aasutosh Jha <aasutossh@gmail.com>
2019-08-09 17:07:31 +0545

    added database for donates

--- app/src/main/java/np/com/aasutosh/bloodbankandroid/FeedActivity.java
+++ app/src/main/java/np/com/aasutosh/bloodbankandroid/FeedActivity.java
@@ -10,14 +10,16 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class FeedActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
-    private DatabaseReference databaseReference;
+    private DatabaseReference databaseRequests, databaseDonates;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("requests");
-        databaseReference.keepSynced(true);
+        databaseRequests = FirebaseDatabase.getInstance().getReference().child("requests");
+        databaseRequests.keepSynced(true);
+        databaseDonates = FirebaseDatabase.getInstance().getReference().child("donates");
+        databaseDonates.keepSynced(true);
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -28,13 +30,12 @@ public class FeedActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         FirebaseRecyclerAdapter<Request, RequestViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Request, RequestViewHolder>
-                (Request.class, R.layout.request_list, RequestViewHolder.class, databaseReference) {
+                (Request.class, R.layout.request_list, RequestViewHolder.class, databaseRequests) {
             @Override
             protected void populateViewHolder(RequestViewHolder requestViewHolder, Request request, int i) {
                 requestViewHolder.setName(request.getName());
                 requestViewHolder.setQuantity(request.getQuantity());
                 requestViewHolder.setBloodGroup(request.getBloodGroup());
-                requestViewHolder.setTypeOfRequest(request.getTypeOfRequest());
                 requestViewHolder.setPhoneNum(request.getPhoneNum());
             }
         };
